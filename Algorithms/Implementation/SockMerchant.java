9383ea235ae01a6c7c449f1ba034f7ed60ecd2ac
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-09-27 13:22:12 +0100

    Create SockMerchant.java

---
+++ Algorithms/Implementation/SockMerchant.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int sockMerchant(int n, int[] ar) {
+        // Complete this function
+        Arrays.sort(ar);
+        int count = 0;
+        for(int i = 0; i < ar.length-1; i++){
+            if(ar[i]==ar[i+1]){
+                i++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+    }
+}

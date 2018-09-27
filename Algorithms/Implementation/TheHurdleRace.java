d20e9c5ac70a9ad69110d3ea7fa96204e4664f7e
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-09-27 13:29:29 +0100

    Create TheHurdleRace.java

---
+++ Algorithms/Implementation/TheHurdleRace.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int hurdleRace(int k, int[] height) {
+        // Complete this function
+        int ans = 0;
+        for(int i = 0; i < height.length; i++)
+            if(height[i] - k > ans)
+                ans = height[i] - k;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i = 0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+        int result = hurdleRace(k, height);
+        System.out.println(result);
+        in.close();
+    }
+}

5dded37203f8d68b76c69c77bcfd8e133e693e49
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-09-27 13:45:14 +0100

    Create InsertionSort-Part 1.java

---
+++ Algorithms/Sorting/InsertionSort-Part 1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void insertionSort1(int n, int[] array) {
+        // Complete this function
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;
+                for(int q = 0; q < n; q++)
+                    System.out.print(array[q] + " ");
+            System.out.println();
+            }  
+            array[i+1] = key; 
+            
+        }
+        for(int q = 0; q < n; q++)
+                    System.out.print(array[q] + " ");
+            System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        insertionSort1(n, arr);
+        in.close();
+    }
+}

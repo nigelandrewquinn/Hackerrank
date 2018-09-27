5f13c62e6c4d7694fcc03abce56f528322a46c5e
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-09-27 13:40:25 +0100

    Create BigSorting.py

---
+++ Algorithms/Sorting/BigSorting.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+unsorted = []
+unsorted_i = 0
+for unsorted_i in range(n):
+   unsorted_t = str(input().strip())
+   unsorted.append(unsorted_t)
+
+unsorted.sort(key=int)
+for item in unsorted:
+	print(item)
+
+
